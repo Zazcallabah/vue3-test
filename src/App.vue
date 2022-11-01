967ed3ea25ebcf75a9b9f837cb3ddd3920a47c2a
Peter Hamberg <zazcallabah@gmail.com>
2022-11-01 11:23:28 +0100

    add failing example with pnicon and table

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,70 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import countryCodes from "@/components/codes";
+import { useStore } from "@/stores/store";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+const store = useStore();
+const { choice } = storeToRefs(store);
+const list = ref(["one", "two", "three"] as string[]);
+const setCountry = (e: any) => {
+  choice.value = e.detail;
+};
+const add = () => {
+  list.value.push("Another" + Math.random());
+};
 </script>
 
 <template>
-  <RouterView />
+  <h2>Issue 1 - pn-select</h2>
+  <p>
+    The pn-select component does not properly display initial value. If you
+    refresh the page, the select will be blank despite the ref having a selected
+    value.
+  </p>
+  <pn-select
+    :label="'country'"
+    id="country"
+    searchable="true"
+    @selectchange="setCountry"
+  >
+    <pn-option
+      v-for="code in countryCodes"
+      :key="code"
+      :selected="code === choice"
+      :value="code"
+    >
+      {{ `countries.${code}` }}
+    </pn-option>
+  </pn-select>
+  <p>you selected: {{ choice }}</p>
+  <hr />
+  <h2>Issue 2 - pn-buttons in v-for pn-table</h2>
+  <button @click="add">Add</button>
+
+  <pn-table bordered="true" striped="true">
+    <table>
+      <thead>
+        <th>button</th>
+        <th>icon</th>
+      </thead>
+      <tbody>
+        <tr v-for="l in list" :key="l">
+          <td>
+            <pn-button>{{ l }}</pn-button>
+          </td>
+          <td>
+            <pn-icon symbol="unfold" color="blue700">Drag</pn-icon>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </pn-table>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+  border: 1px solid black;
+  margin: 1rem;
+  padding: 1rem;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
-import router from "./router";
+import { PnIcon } from "@postnord/web-components/components/pn-icon";
+customElements.define("pn-icon", PnIcon);
+import { PnTable } from "@postnord/web-components/components/pn-table";
+customElements.define("pn-table", PnTable);
 
+import { PnButton } from "@postnord/web-components/components/pn-button";
+customElements.define("pn-button", PnButton);
 import { PnOption } from "@postnord/web-components/components/pn-option";
 customElements.define("pn-option", PnOption);
 import { PnSelect } from "@postnord/web-components/components/pn-select";
@@ -12,6 +17,5 @@ customElements.define("pn-select", PnSelect);
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
 
 app.mount("#app");
