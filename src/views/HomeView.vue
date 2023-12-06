7499b680dab291deac58753578189c6e80055443
Peter Hamberg <zazcallabah@gmail.com>
2023-12-06 10:23:56 +0100

    remove unused stuff from demo page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,11 @@
 import countryCodes from "@/components/codes";
 import { useStore } from "@/stores/store";
 import { storeToRefs } from "pinia";
-import { ref } from "vue";
-import { wrench } from "pn-design-assets/pn-assets/icons";
 
 const store = useStore();
 const { choice } = storeToRefs(store);
 const setCountry = (e: any) => {
   choice.value = e.detail;
-};
-const loading = ref(false);
-const onSearch = () => {
-  loading.value = true;
-  setTimeout(() => {
-    loading.value = false;
-  }, 5000);
 };
 </script>
 
