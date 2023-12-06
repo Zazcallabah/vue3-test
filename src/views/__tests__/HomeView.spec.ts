b6c137a409c2ccee3b083e149ee006fb11f9cda1
Peter Hamberg <zazcallabah@gmail.com>
2023-12-06 10:38:11 +0100

    fix broken unit test pinia

--- src/views/__tests__/HomeView.spec.ts
+++ src/views/__tests__/HomeView.spec.ts
@@ -1,9 +1,16 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 
 import { mount } from "@vue/test-utils";
 import HelloWorld from "../HomeView.vue";
+import { setActivePinia, createPinia } from "pinia";
 
 describe("HelloWorld", () => {
+  beforeEach(() => {
+    // creates a fresh pinia and makes it active
+    // so it's automatically picked up by any useStore() call
+    // without having to pass it to it: `useStore(pinia)`
+    setActivePinia(createPinia());
+  });
   it("renders properly", () => {
     const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
     expect(wrapper.text()).toContain("Issue");
