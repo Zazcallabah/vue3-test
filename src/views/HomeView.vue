8f5dce678a010c8109e8eee6badd385f37b3c202
Peter Hamberg <zazcallabah@gmail.com>
2023-12-07 16:00:58 +0100

    add test for vue-debounce

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import countryCodes from "@/components/codes";
 import { useStore } from "@/stores/store";
 import { storeToRefs } from "pinia";
 import { wrench } from "pn-design-assets/pn-assets/icons";
+import { vue3Debounce } from "vue-debounce";
+const vDebounce = vue3Debounce({});
 
 const store = useStore();
 const { choice } = storeToRefs(store);
 const setCountry = (e: any) => {
   choice.value = e.detail;
+};
+const decount = ref(0);
+const debounced = () => {
+  decount.value++;
 };
 </script>
 
@@ -40,6 +47,10 @@ const setCountry = (e: any) => {
     <h2>Test 2 - icons</h2>
     <pn-icon :symbol="wrench" />
   </section>
+  <section>
+    <h2>Test 3 - debounce</h2>
+    <input v-debounce:800ms="debounced" /> Debounce count is {{ decount }}
+  </section>
 </template>
 
 <style scoped>
@@ -47,6 +58,7 @@ section {
   max-width: 500px;
   margin: 5rem auto;
 }
+
 div {
   border: 1px solid black;
   margin: 1rem;
